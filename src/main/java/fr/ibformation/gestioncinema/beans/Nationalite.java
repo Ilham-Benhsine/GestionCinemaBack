9342e80d603c05397068677ff5d1f68dca6a57e8
AntoineAubree <antoine.aubree@protonmail.com>
2020-08-13 21:24:42 +0200

    create Nationalite

---
+++ src/main/java/fr/ibformation/gestioncinema/beans/Nationalite.java
@@ -0,0 +1,21 @@
+package fr.ibformation.gestioncinema.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Nationalite {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String valeur;
+
+}
