cd502e82ce89ba3794f4014625c80f641e538c25
Antoine Aubree <antoine.aubree@protonmail.com>
2020-08-21 14:28:05 +0200

    add crossOrigin

--- src/main/java/fr/ibformation/gestioncinema/controller/FilmController.java
+++ src/main/java/fr/ibformation/gestioncinema/controller/FilmController.java
@@ -1,6 +1,7 @@
 package fr.ibformation.gestioncinema.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class FilmController {
 	@Autowired
 	FilmManager filmManager;
 	
+	@CrossOrigin
 	@RequestMapping(method = RequestMethod.GET, path = ("/"))
 	public Iterable<Film> getPersonne() {
 		return filmManager.findAll();
